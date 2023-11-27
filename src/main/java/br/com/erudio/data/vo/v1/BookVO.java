201361303cdf219b65c665692cdd491c35cde190
hugosilva-dev <eahugosilva@gmail.com>
2023-11-27 00:03:01 -0300

    Inserção da entidade Book, BookVO, Repository e as migrations para auxiliar na criação das tabelas no banco de dados

---
+++ src/main/java/br/com/erudio/data/vo/v1/BookVO.java
@@ -0,0 +1,90 @@
+package br.com.erudio.data.vo.v1;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Objects;
+
+import org.springframework.hateoas.RepresentationModel;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.github.dozermapper.core.Mapping;
+
+@JsonPropertyOrder({"id", "title", "author", "launchDate", "price"})
+public class BookVO extends RepresentationModel<BookVO> implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@JsonProperty("id")
+	@Mapping("id")
+	private Long key;
+	private String author;
+	private LocalDate launchDate;
+	private BigDecimal price;
+	private String title;
+	
+	public BookVO() {}
+
+	public Long getKey() {
+		return key;
+	}
+
+	public void setKey(Long key) {
+		this.key = key;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public LocalDate getLaunchDate() {
+		return launchDate;
+	}
+
+	public void setLaunchDate(LocalDate launchDate) {
+		this.launchDate = launchDate;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Objects.hash(author, key, launchDate, price, title);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BookVO other = (BookVO) obj;
+		return Objects.equals(author, other.author) && Objects.equals(key, other.key)
+				&& Objects.equals(launchDate, other.launchDate) && Objects.equals(price, other.price)
+				&& Objects.equals(title, other.title);
+	}
+}
---
+++ src/main/java/br/com/erudio/model/Book.java
@@ -0,0 +1,103 @@
+package br.com.erudio.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class Book implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String author;
+	
+	@Column(nullable = false)
+	private LocalDate launchDate;
+	
+	@Column(nullable = false)
+	private BigDecimal price;
+	
+	@Column(nullable = false)
+	private String title;
+	
+	public Book() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public LocalDate getLaunchDate() {
+		return launchDate;
+	}
+
+	public void setLaunchDate(LocalDate launchDate) {
+		this.launchDate = launchDate;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(author, id, launchDate, price, title);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		return Objects.equals(author, other.author) && Objects.equals(id, other.id)
+				&& Objects.equals(launchDate, other.launchDate) && Objects.equals(price, other.price)
+				&& Objects.equals(title, other.title);
+	}
+	
+	
+	
+	
+	
+	
+}
